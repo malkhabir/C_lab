ef7391bfc65eac9ef7b1b4820d60508890b4afec
Mahamad A. Kanoute <44517204+malkhabir@users.noreply.github.com>
2019-10-14 23:59:59 -0300

    Pendu
    Little game while experimenting with the language

---
+++ cmake-build-debug/CMakeFiles/pendu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pendu.dir/main.c.o"
+  "pendu.pdb"
+  "pendu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pendu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
